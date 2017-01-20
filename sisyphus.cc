5bfb4c2e9e2744dab679bf0ba243602d236fbee4
Eymen Kurdoglu <eymenkurdoglu@gmail.com>
2017-01-20 16:01:32 -0500

    arrays

--- sisyphus.cc
+++ sisyphus.cc
@@ -1,13 +1,30 @@
 #include <iostream>
-#include "llist.hh"
+#include <cstdlib>
+#include <vector>
+#include <ctime>
+
 #include "bt.hh"
+#include "llist.hh"
+#include "stack.hh"
 
 using namespace std;
 
 int main ()
 {
 
-llist a( 5 );
+int N; cin >> N;
+
+srand( time( NULL ) );
+
+vector<int> v;
+
+for ( int i = 0; i < N; i++ )
+	v.push_back( rand()%20 );
+
+for ( int i : v ) cout << i << ' ';
+cout << endl;
+
+
 
 return 0;
 }
---
+++ stack.hh
@@ -0,0 +1,10 @@
+#ifndef STACK_HH
+#define STACK_HH
+#include "llist.hh"
+
+class stack : public llist {
+
+
+
+};
+#endif
